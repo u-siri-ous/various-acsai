0be4fc991cc29d15c3e7d947d97a0f5189ed6408
u-siri-ous <sanninosiria@gmail.com>
2023-03-08 10:13:47 +0100

    mon 6 - tue 7 lectures

---
+++ opencv_lec1.py
@@ -0,0 +1,16 @@
+import cv2
+
+img = cv2.imread('dajeroma.jpg')    #reads image
+
+width = img.shape[1]    #.shape returns a tuple
+height = img.shape[0]
+channels = img.shape[2]
+
+print(width, height, channels)
+
+cv2.imshow('maggggica', img)    #shows image. syntax .imread(title, image)
+cv2.waitKey()   #keeps image until time (if specified, in milliseconds) expires
+
+#cv2.destroyAllWindows() #useful in jupyterlab
+
+cv2.imwrite('laromaaaaaaaaaaa.jpg', img)    #first argument is title (with extension that can be different from imread), second is the image
