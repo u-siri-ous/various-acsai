30ab992ddf875cbd73f90902de28debe94862b8e
Siria Sannino <sanninosiria@gmail.com>
2023-05-31 12:11:19 +0200

    last lecture :c

--- pytorch_ml/cnn_no_sequential.py
+++ pytorch_ml/cnn_no_sequential.py
@@ -7,69 +7,40 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
-# FCN - fully convolutional network 
-# lacks the multilayer perceptron layer, used for segmentation tasks 
-# classification is made pixelwise by using 1x1 convolutions 
-
-# import train and testing data
 training_data = datasets.FashionMNIST(root='data', train=True, download=True, transform=ToTensor()) 
 test_data = datasets.FashionMNIST(root='data', train=False, download=True, transform=ToTensor()) 
 
-""" # labels of the dataset (also found online)
-labels_map = {
-    0: 'tshirt',
-    1: 'trousers',
-    2: 'pullover',
-    3: 'dress',
-    4: 'coat',
-    5: 'sandal',
-    6: 'shirt',
-    7: 'sneaker',
-    8: 'bag',
-    9: 'ankle boot'
-} """
-
-# create the model / neural network
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
-# define our CNN
-# the number of channels is important
-
-# shape of tensor: (batch, channels, width, height)
 class OurCNN(nn.Module):
     def __init__(self):
+        # store everything we're gonna use
         super().__init__()
-        self.cnn = nn.Sequential(
-            # convolutional layer
-            nn.Conv2d(1, 5, kernel_size=3),  # input channels == number of channels of input image, we do not care about the size of the image
-            # (64, 1, 28, 28)
-            nn.ReLU(),
-            nn.Conv2d(5, 10, kernel_size=3), # MLP technique: input == output
-            # (64, 5, 26, 26)
-            # w = h = 26 because if we do not use padding and stride, output for each dimension is dimension - kernel_size + 1
-            nn.ReLU(),
-            # adding pooling to redure dimensionality, from 24*24*10 to 12*12*10
-            nn.MaxPool2d(kernel_size=2)
-        )
-        self.mlp = nn.Sequential(
-            # multilayer perceptron layer for classification
-            # nn.Flatten(),    # can be done here on in forward(self, x)
-            nn.Linear(12*12*10, 10),   # used torch.size (x.shape) to get correct value after flatten in forward
-            nn.ReLU(),
-            nn.Linear(10, 10)
-        )
 
+        self.input_layer = nn.Conv2d(1, 5, kernel_size=3)
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(5, 10, kernel_size=3) 
+        self.max_pool = nn.MaxPool2d(kernel_size=2)
+        self.input_linear = nn.Linear(12*12*10, 10)   
+        self.output_linear = nn.Linear(10, 10)
+
+    # create the same model as cnn.py without using Sequential()
     def forward(self, x):
-        x = self.cnn(x)
-        # x.shape
-        x = torch.flatten(x, 1)     # with dimension 1 (array)
-        # x.shape
-        x = self.mlp(x)
+        x = self.input_layer(x)
+        x = self.relu(x)
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.max_pool(x)
+
+        x = torch.flatten(x, 1)     
+
+        x = self.input_linear(x)
+        x = self.relu(x)
+        x = self.output_linear(x)
         return x
     
 model = OurCNN().to(device)
 
-# define hyperparameters (the model cannot learn them) - the holy three - play with these
 epochs = 3                                             # how many times should our model analyze the dataset
 batch_size = 64                                        # number of samples that we get from the dataset each time (lower if crash)
 learning_rate = 0.001                                  # amount of change allowed on weights in backpropagation (changeable in various epochs in certain techniques)
@@ -78,15 +49,12 @@ learning_rate = 0.001                                  # amount of change allowe
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-# we have to define the training loop (automatically done in sklearn)
-# we aim for variability in each epoch, so choosing to increment epochs vs batch_size depends on the dataset
 
 # define the loss function - computes the differences between our prediction and the correct labels and is used to update weights on backpropagation
 loss_fn = nn.CrossEntropyLoss()     # uses negative log likelihood
 
 # define the optimizer - the mathematical approach used to compute the gradient
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)   # stochastic gradient descent, model.parameters() are the weights
-# adamw is faster and better than sgd, play with hyperparameters as well
 
 # create accuracy metric
 metric = torchmetrics.Accuracy(task='multiclass', num_classes=10)   # multiclassification for 10 classes
@@ -95,11 +63,8 @@ metric = torchmetrics.Accuracy(task='multiclass', num_classes=10)   # multiclass
 def train_loop(dataloader, model, loss_fn, optimizer):    #get samples from dataset, model, loss fn, optimizer
     # get batch from dataset (X=data, y=label)
     for batch, (X,y), in enumerate(dataloader):
-        # size = len(dataloader)      # sample in datasets, not needed with metric
-
         # compute prediction and loss
         pred = model(X)
-        #y_tensor = torch.tensor([y])       # AS LIST SENNO SCOPPIA
         loss = loss_fn(pred, y)     # difference between true and predicted (tensors)
 
         # backward pass (backpropagation)
@@ -111,7 +76,7 @@ def train_loop(dataloader, model, loss_fn, optimizer):    #get samples from data
         if batch % 100 == 0:        # every 100 iterations
             acc = metric(pred, y)
             print(f'current batch accuracy: {acc}')
-            
+
     # final training accuracy
     acc = metric.compute()
     print(f'epoch accuracy: {acc}')
@@ -123,9 +88,6 @@ def test_loop(dataloader, model, loss_fn):      # no optimizer cuz we don't need
     with torch.no_grad():
         for X, y in dataloader:
             pred = model(X)
-            #y_tensor = torch.tensor([y])
-            test_loss += loss_fn(pred, y).item()    # compute loss and add it to total loss
-            correct += (pred.argmax(1)).type(torch.float).sum().item()  # get all maximum values for the whole batch, then sum them and get the number
             
             # print accuracy and avg. loss, as we have accuracy only for the current batch
             acc = metric(pred, y)
---
+++ pytorch_ml/lstm_rnn.py
@@ -0,0 +1,60 @@
+# RNN - recurrent neural network
+# rnns do not work well with long sequences because of recursivity
+
+# vanishing gradient problem
+
+# exploding gradient problem 
+
+# LSTM - Long-Short Term Memory
+# ability to learn from temporal data
+
+# inside a single lstm node there are three smaller nodes:
+# input gate
+# forget gate
+# output gate
+
+import torch
+from torch import nn
+
+'''
+input formatting for lstm:
+
+input, h_0, c_0 
+
+input = data
+h_0 = initial hidden state of elements in input data
+c_0 = initial cell state(s)
+
+#########
+
+input = (L, N, H_in)
+L = len(sequence)
+N = batch size
+H_in = size of input data
+
+h_0 = (D, N, H_out)
+D = shape, number of directions (as we can have cycles in lstm
+                                ; if set to 1, it works like a normal nn)
+N = optional, as before
+H_out = features we want as output
+                                
+c_0 = (D, N, num_cells)
+D = same as before
+N = optional, as before
+num_cells = number of wanted cells (usually equal to H_out)
+'''
+
+# create a single lstm node
+lstm_node = nn.LSTM(10, 20, 1)       
+# LSTM(input size, outputs, number of lstm cells that we wanna start together)
+
+# create the input
+input_data = torch.randn(5, 3, 10)
+h_0 = torch.randn(1, 3, 20)
+c_0 = torch.randn(1, 3, 20)
+
+output, (h_n, c_n) = lstm_node(input_data, (h_0, c_0))
+
+print(output.shape)
+
+# to train it, it works the same as before
